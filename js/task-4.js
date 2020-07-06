91fe89a3401cab3e6085a920894b2defa479440f
Yurii Sokolov <nintendowc3@gmail.com>
2020-07-06 12:28:21 +0300

    Modyl-1

--- js/task-4.js
+++ js/task-4.js
@@ -1,2 +1,26 @@
 let credits = 23580;
-let pricePerDroid = 3000;
+let pricePerDroir = 3000;
+let totalPrice;
+let result;
+
+let count = window.prompt();
+if (count === null) {
+  result = 'Отменено пользователем!';
+} else if (isNaN(count[0]) || isNaN(count)) {
+  result = 'Введите число';
+} else {
+  totalPrice = pricePerDroir * count;
+}
+
+if (!result) {
+  if (credits - totalPrice <= 0) {
+    window.console.log('Недостаточно средств на счету!.');
+  } else {
+    credits = credits - totalPrice;
+    window.console.log(
+      `Вы купили ${count} дроида(ов), на счету осталось ${credits}`,
+    );
+  }
+} else {
+  window.console.log(result);
+}
